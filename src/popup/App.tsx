daab5bfda17e84e90c0a70ba77d9a87121888b51
jonlamb <jonnylamb@gmail.com>
2021-01-24 14:14:21 -0600

    Adding sorting logic

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useLayoutEffect, useState } from 'react';
 import { Flipper } from 'react-flip-toolkit';
 import db from '../firebase';
 import { IAccount, IFirebaseAccount, IFirebaseUser, IUser } from '../../typings';
@@ -17,10 +17,7 @@ const App: FC = () => {
         const firebaseUser = doc.data() as IFirebaseUser;
         users.push({ ...firebaseUser, lastSeenDate: firebaseUser.lastSeenDate ? firebaseUser.lastSeenDate.toDate().toUTCString() : '' });
       });
-      const sortedUsers = users.sort((a, b) => {
-        return a.status.localeCompare(b.status) || new Date(b.lastSeenDate).getTime() - new Date(a.lastSeenDate).getTime();
-      });
-      setUsers(() => sortedUsers);
+      setUsers(() => users);
     });
     return unsubscribe;
   }, []);
@@ -30,21 +27,31 @@ const App: FC = () => {
       const accounts: IAccount[] = [];
       snapshot.forEach((doc) => {
         const firebaseAccount = doc.data() as IFirebaseAccount;
-        if (firebaseAccount.accountNum !== '3499441') accounts.push({ ...firebaseAccount, lastSeenDate: firebaseAccount.lastSeenDate.toDate().toUTCString(), id: doc.id });
+        if (firebaseAccount.accountNum !== '3499441') accounts.push({ ...firebaseAccount, lastSeenDate: firebaseAccount.lastSeenDate.toDate().toUTCString() });
       });
-      // TODO: sort to minute and then by name
       setAccounts(() => accounts);
     });
     return unsubscribe;
   }, []);
 
-  useEffect(() => {
-    // TODO: sorty by hasactiveuser and then by last seen date and then by name
-    // const sortedAccounts;
-  }, [users]);
+  const sortedAccounts = accounts.sort(sortAccounts);
+  const sortedUsers = users.sort(sortUsers);
+
+  function sortAccounts(a: IAccount, b: IAccount): number {
+    const aHasActiveUsers = users.filter((user) => user.status === 'active').map((user) => user.account?.id).includes(a.accountNum);
+    const bHasActiveUsers = users.filter((user) => user.status === 'active').map((user) => user.account?.id).includes(b.accountNum);
+    const activeUserComparison = aHasActiveUsers === bHasActiveUsers ? 0 : aHasActiveUsers ? -1 : 1;
+    const lastSeenComparison = roundToMinutes(1, b.lastSeenDate) - roundToMinutes(1, a.lastSeenDate);
+    const accountNameComparison = a.accountName.localeCompare(b.accountName);
+    return activeUserComparison || lastSeenComparison || accountNameComparison;
+  }
+
+  function sortUsers(a: IUser, b: IUser): number {
+    return a.status.localeCompare(b.status) || new Date(b.lastSeenDate).getTime() - new Date(a.lastSeenDate).getTime();
+  }
 
   const Loader = () => {
-    return(
+    return (
       <div className='spinner transition-shown center'>
         <div className='bounce1'></div>
         <div className='bounce2'></div>
@@ -58,19 +65,26 @@ const App: FC = () => {
   return (
     <div className='container'>
       <Flipper flipKey={accounts.map((account) => account.accountNum).join('')}>
-        {/* <button onClick={shuffleList}> shuffle</button> */}
-        <div className='flex-container'>
-          <div className='center'><h1>NetSuite Account Status</h1></div>
-          {accounts.length === 0 && users.length === 0
-            ? <Loader></Loader>
-            : accounts.map(account => {
-              const relatedUsers = users.filter((user) => user.account?.id === account.id);
-              return <Card key={account.accountNum} users={relatedUsers} {...account}></Card>;
-            })}
-        </div>
+      {/* <button onClick={shuffleList}> shuffle</button> */}
+      <div className='flex-container'>
+        <div className='center'><h1>NetSuite Account Status</h1></div>
+        {accounts.length === 0 && users.length === 0
+          ? <Loader></Loader>
+          : sortedAccounts.map((account) => {
+            const relatedUsers = sortedUsers.filter((user) => user.account?.id === account.accountNum);
+            return <Card key={account.accountNum} users={relatedUsers} {...account}></Card>;
+          })}
+      </div>
       </Flipper>
     </div>
   );
 };
+
+function roundToMinutes(minutes: number, dateStr: string): number {
+  const d = new Date(dateStr);
+  let ms = 1000 * 60 * minutes; // convert minutes to ms
+  let roundedDate = new Date(Math.floor(d.getTime() / ms) * ms);
+  return roundedDate.getTime();
+}
 
 export default App;
